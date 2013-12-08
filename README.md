02e6634b504726add3c220e004756ceb9074237a
Koen Ongena <gijoke@gmail.com>
2013-12-08 22:22:33 +0100

    Still working on it...

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 puppet-sample
 =============
 
-puppet-sample
+Installation
+
+1. install pupppet 
+2. puppet module install biemond/jdk7
+3. puppet module install puppetlabs/apt
+4. puppet module install puppetlabs/mysql
+5. puppet module install puppetlabs/mongodb
+6. puppet module install erwbgy/jboss
+7. mkdir -p /etc/puppet/modules/jdk7/files 
+8. cd /etc/puppet/modules/jdk7/files
+9. wget --no-cookies --header 'Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com' 'http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz' --no-check-certificate
--- jboss.pp
+++ jboss.pp
@@ -1,46 +1,59 @@
-include jdk7
-
 jdk7::install7{ 'jdk1.7.0_45':
     version              => "7u45" , 
     fullVersion          => "jdk1.7.0_45",
     alternativesPriority => 18000, 
-    x64                  => false,
+    x64                  => true,
     downloadDir          => "/install",
     urandomJavaFix       => false,
     sourcePath           => "puppet:///modules/jdk7/"
 }
 
+include jdk7
+
 class { 'jdk7::urandomfix' :}
 
-class { 'apt':
-  always_apt_update    => true,
-  disable_keys         => undef,
-  proxy_host           => false,
-  proxy_port           => '8080',
-  purge_sources_list   => false,
-  purge_sources_list_d => false,
-  purge_preferences_d  => false,
-  update_timeout       => undef
-}
+# class { 'apt':
+  # always_apt_update    => true,
+  # disable_keys         => undef,
+  # proxy_host           => false,
+  # proxy_port           => '8080',
+  # purge_sources_list   => false,
+  # purge_sources_list_d => false,
+  # purge_preferences_d  => false,
+  # update_timeout       => undef
+# }
 
-apt::source { 'percona':
-  location   => 'http://repo.percona.com/apt',
-  repos      => 'main',
-  key        => '1C4CBDCDCD2EFD2A',
-  key_server => 'keys.gnupg.net',
-  include_src => true
-}
+ # apt::source { 'percona':
+   # location   => 'http://repo.percona.com/apt',
+   # release    => 'stable',
+   # repos      => 'main',
+   # key        => '1C4CBDCDCD2EFD2A',
+   # key_server => 'keys.gnupg.net',
+   # include_src => true
+ # }
 
-include apt
+ # include apt
+ 
+ yumrepo { "percona":
+  name    	=> "Percona",
+  descr		=> "Percona Server",
+  baseurl	=> "http://repo.percona.com/centos/\$releasever/os/\$basearch/",
+  enabled 	=> 1,
+  gpgcheck   => 0,
+}
 
 file { '/etc/init.d/mysqld':
    ensure => 'link',
    target => '/etc/init.d/mysql',
 }
+
+file { '/var/lib/mysql/localhost.localdomain.pid':
+	target => '/var/lib/mysql/localhost.localdomain.pid'
+}
  
 class { 'mysql::server': 
-  require 	=> [ Apt::Source["percona"], File['/etc/init.d/mysqld'] ],
-  package_name => 'percona-server-server-5.5',
+  require 	=> [ Yumrepo["percona"], File['/etc/init.d/mysqld'] ],
+  package_name => 'Percona-Server-server-56.x86_64',
   package_ensure => latest,
   service_name => 'mysql',
   override_options => { 'mysqld' => {
@@ -59,3 +72,11 @@ class { 'mysql::server':
 }
 
 include '::mysql::server'
+
+# class { 'mongodb':
+  # init => 'sysv',
+# }
+
+# include mongodb
+
+#include jboss
